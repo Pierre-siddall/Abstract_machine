86bc74a4143e02d0370f5b55451f0ab5a931137d
Pierre Siddall <43399998+Pierre-siddall@users.noreply.github.com>
2021-10-31 19:29:56 +0000

    Add files via upload

--- coursework/src/Machine.hs
+++ coursework/src/Machine.hs
@@ -1,4 +1,3 @@
-{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 module Machine
 (
         Vname,
@@ -25,30 +24,32 @@ data Instr = LOADI Int | LOAD Vname | ADD |STORE Vname | JMP Int | JMPLESS Int |
         deriving (Eq, Read, Show)
 
 --TODO Task 1.5
-type Stack =[Int]
+type Stack =[Val]
 
 --TODO Task 1.6
 type Config = (Int,State,Stack)
 
---TODO Task 1.7
+ --TODO Task 1.7
 iexec :: Instr -> Config -> Config
 
-iexec (LOADI x) (a,b,c) =(a+1,b,x:c) --Done
+iexec (LOADI x) (a,b,c) =(a+1,b,x:c) 
 
-iexec (LOAD v) (a,b,c) =(a+1,b,[y | (x,y)<-head b])
+iexec (LOAD v) (a,b,c) =(a+1,b,[ x | Just x <- [Data.Map.lookup v b]]) 
 
-iexec ADD (a,b,c)=(a+1,b,[Prelude.sum (Prelude.take 2 c)]) --Done
+iexec ADD (a,b,c)=(a+1,b,[Prelude.sum (Prelude.take 2 c)]) 
 
---iexec (STORE v) (a,b,c) =(a+1,Data.Map v head c,Prelude.drop 1 c)
+iexec (STORE v) (a,b,c) =(a+1,Data.Map.insert v (head c) b,Prelude.drop 1 c) 
 
-iexec (JMP i) (a,b,c) =(a+i,b,c) --Done
+iexec (JMP i) (a,b,c) =(a+i+1,b,c) 
 
-iexec (JMPLESS i) (a,b,c) = if head(tail c)<head c then (a+1+i,b,Prelude.drop 2 c) else (a+1,b,Prelude.drop 2 c)--Done 
+iexec (JMPLESS i) (a,b,c) = if head(tail c)<head c then (a+1+i,b,Prelude.drop 2 c) else (a+1,b,Prelude.drop 2 c) 
 
 
-iexec (JMPGE i) (a,b,c) =if head(tail c)>=head c then (a+1+i,b,Prelude.drop 2 c) else (a+1,b,Prelude.drop 2 c) --Done 
+iexec (JMPGE i) (a,b,c) =if head(tail c)>=head c then (a+1+i,b,Prelude.drop 2 c) else (a+1,b,Prelude.drop 2 c)  
 
-iexec (IUndefined) (a,b,c) = undefined --Done 
+iexec IUndefined (a,b,c) = undefined  
 
 exec :: [Instr] -> Config -> Config
-exec xs (a,b,c)= (Prelude.length xs,b,c)  
+exec xs (a,b,c) | length xs==1 = iexec (head xs) (a,b,c)
+                | otherwise = exec (tail xs) (iexec (head xs) (a,b,c))
+
